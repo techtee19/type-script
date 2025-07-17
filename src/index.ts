5dbf64af9c10fea259add0a2823cfd060e32e59d
techtee19 <techtee19@gmail.com>
2025-07-17 15:00:21 +0100

    Update TypeScript configuration to enable unused locals, parameters, and implicit returns checks

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,13 @@ const enum Size {
 }
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+// Functions
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+calculateTax(10_000);
