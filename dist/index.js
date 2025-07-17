4df256085a7508a9b902459b2d5b034309f855e1
techtee19 <techtee19@gmail.com>
2025-07-17 14:41:35 +0100

    Refactor index.ts to reorganize variable declarations and comment out the render function

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+let sales = 123456789;
+let course = "TypeScript Fundamentals";
+let isPublished = true;
+let level;
+let numbers = [1, 2, 3];
+let user = [1, "Tega"];
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,21 @@ let course = "TypeScript Fundamentals";
 let isPublished = true;
 let level;
 
-function render(document) {
-  console.log(document);
-}
+// function render(document) {
+//   console.log(document);
+// }
 
 // arrays
 let numbers: number[] = [1, 2, 3];
+
+// Tuples: a fixed length array with known types
+let user: [number, string] = [1, "Tega"];
+
+// Enums: a list ot related constants
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
