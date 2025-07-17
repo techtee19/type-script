a2f0a65e76b09abfc4fb3dba654eb81104fc7310
techtee19 <techtee19@gmail.com>
2025-07-17 14:24:21 +0100

    Refactor index.ts to move the render function and numbers array declaration

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ let sales = 123_456_789;
 let course = "TypeScript Fundamentals";
 let isPublished = true;
 let level;
+
+function render(document) {
+  console.log(document);
+}
+
+// arrays
+let numbers: number[] = [1, 2, 3];
