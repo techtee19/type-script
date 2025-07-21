8875fa7bc610db91a3052c7a90510f413f048da8
techtee19 <techtee19@gmail.com>
2025-07-21 16:54:29 +0100

    Add generic utility functions and interfaces for enhanced type safety

--- src/generics.ts
+++ src/generics.ts
@@ -5,3 +5,34 @@ class KeyVAluePair<K, V> {
 let pair = new KeyVAluePair<string, number>("age", 30);
 
 // GENERIC FUNCTIONS
+class ArrayUtils {
+  static wrapInArray<T>(value: T) {
+    return [value];
+  }
+}
+
+let number = ArrayUtils.wrapInArray(1);
+
+// GENERIC INTERFACES
+interface Result<T> {
+  data: T | null;
+  error: string | null;
+}
+
+function fetch<T>(url: string): Result<T> {
+  return { data: null, error: null };
+}
+
+interface User {
+  username: string;
+}
+
+interface Product {
+  title: string;
+}
+
+let result = fetch<User>("url");
+result.data?.username;
+
+let results = fetch<Product>("url");
+results.data?.title;
