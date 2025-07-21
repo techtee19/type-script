081bb2a38cdfb76f35e86e6a1475f98f7f544255
techtee19 <techtee19@gmail.com>
2025-07-21 08:19:04 +0100

    Refactor Account class to use private balance property and add error handling; implement Ride class with static activeRides tracking

--- dist/oop.js
+++ dist/oop.js
@@ -1,19 +1,45 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error("Invalid amount");
-        this.balance += amount;
+        this._balance += amount;
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(value) {
+        if (value < 0)
+            throw new Error("Invalid value");
+        this._balance = value;
     }
 }
 let account = new Account(1, "Tega", 100);
 account.deposit(1200);
-console.log(account.balance);
-console.log(typeof account);
 console.log(account instanceof Account);
+console.log(account.balance);
+class SeatAssignent {
+}
+let seats = new SeatAssignent();
+seats.A1 = "Mosh";
+seats.A2 = "John";
+class Ride {
+    start() {
+        Ride.activeRides++;
+    }
+    stop() {
+        Ride.activeRides--;
+    }
+}
+Ride.activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=oop.js.map
--- src/oop.ts
+++ src/oop.ts
@@ -46,3 +46,27 @@ class SeatAssignent {
 let seats = new SeatAssignent();
 seats.A1 = "Mosh";
 seats.A2 = "John";
+
+// STATIC MEMBERS
+class Ride {
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
+
+  static get activeRides() {
+    return Ride._activeRides;
+  }
+}
+
+let ride1 = new Ride();
+ride1.start();
+
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
